529a25a0a0b1df9daaf22e7d96019e688f92b686
Sora Sofyan <sora.sofyan97@gmail.com>
2020-01-08 10:03:57 +0200

    test

--- app/Http/Controllers/Material.php
+++ app/Http/Controllers/Material.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Certificate extends Controller
+class Material extends Controller
 {
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/Unit.php
+++ app/Http/Controllers/Unit.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Certificate extends Controller
+class Unit extends Controller
 {
     /**
      * Display a listing of the resource.
