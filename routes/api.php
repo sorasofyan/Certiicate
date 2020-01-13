4d7e9d201440c642c237123b8a1ad1008f691511
Sora Sofyan <sora.sofyan97@gmail.com>
2020-01-13 13:07:48 +0200

    edit

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('certificate','CertificateController');
+Route::resource('certificates','CertificateController');
 Route::resource('materials','MaterialController');
-Route::resource('unit','UnitController');
-Route::resource('question','QuestionController');
-Route::resource('answer','AnswerController');
+Route::resource('units','UnitController');
+Route::resource('questions','QuestionController');
+Route::resource('answers','AnswerController');
